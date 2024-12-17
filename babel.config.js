0f57907e2788db3ae1622df3e1dffbab10c69aef
EunjeongHur <ungdunge2@gmail.com>
2024-12-16 16:37:49 -0800

    added .env

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: ["babel-preset-expo"],
-		plugins: ["react-native-reanimated/plugin"],
+		plugins: [
+			"react-native-reanimated/plugin", // 기존 플러그인
+			["module:react-native-dotenv"], // 추가된 dotenv 플러그인
+		],
 	};
 };
--- utils/api.ts
+++ utils/api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
+import { BASEURL } from "@env";
 
 const api = axios.create({
-    //baseURL: "http://10.0.2.2:8080",
-    baseURL: "http://localhost:8080",
-    timeout: 10000,
-    headers: {
-        "Content-Type": "application/json",
-    },
+	baseURL: BASEURL,
+	timeout: 10000,
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 export default api;
